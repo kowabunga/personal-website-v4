1fdc4cfc3844b3330e05fe1cfc9f3099bab5bc80
Anthony Siletti <asiletti@icloud.com>
2023-02-09 16:18:24 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <header class='w-full flex flex-center flex-column'>
+        <div>
+          <img class='profile' src='https://i.imgur.com/0SeONpn.png' />
+        </div>
+      </header>
+      <main></main>
+    </>
+  );
+}
+
+export default App;
